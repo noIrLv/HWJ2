c37b64267035382bb7052f92e8a69f11e6e031f6
Михаил Новиков <spore125@gmail.com>
2023-01-16 16:10:56 +0300

    initian commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus = 20;
+        int amount = 47039;
+
+        int miles = amount / bonus;
+        System.out.println("Бонусные мили за покупку: " + miles);
+
+
+
+
+
+
+
+    }
+
+
+}
